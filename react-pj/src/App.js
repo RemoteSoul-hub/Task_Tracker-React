b194c7f9c9f7fe720b6fb0a1a77bcaf3eafeb838
RemoteSoul-hub <74861627+RemoteSoul-hub@users.noreply.github.com>
2021-05-02 16:43:26 +0000

    First Commit - Base React Files

---
+++ react-pj/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+    
+      <h1>Good day</h1>
+      <h2></h2>
+    </div>
+  );
+}
+
+export default App;
